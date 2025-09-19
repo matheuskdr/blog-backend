bcacd1637b7f1477b29ef22002acf0cfeee01126
matheuskdr <matheusgabriellkpo@gmail.com>
2025-09-19 14:18:39 -0300

    user creation process

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,3 +1,34 @@
 import { RequestHandler } from "express";
+import z from "zod";
+import { createUser } from "../services/user";
+import { createToken } from "../services/auth";
 
-export const signup: RequestHandler = async (req, res) => {};
+export const signup: RequestHandler = async (req, res) => {
+  const schema = z.object({
+    name: z.string(),
+    email: z.email(),
+    password: z.string(),
+  });
+  const data = schema.safeParse(req.body);
+  if (!data.success) {
+    res.json({ error: data.error.flatten().fieldErrors });
+    return;
+  }
+
+  const newUser = await createUser(data.data);
+  if (!newUser) {
+    res.json({ error: "Error ao criar usuário" });
+    return;
+  }
+
+  const token = createToken(newUser);
+
+  res.status(201).json({
+    user: {
+      id: newUser.id,
+      name: newUser.name,
+      email: newUser.email,
+    },
+    token,
+  });
+};
